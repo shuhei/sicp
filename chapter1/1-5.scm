c97b99e842b9729f5751cdd069b0ae82d0d44b08
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-11-11 17:07:40 +0900

    Correct evaluation orders.

--- chapter1/1-5.scm
+++ chapter1/1-5.scm
@@ -5,13 +5,13 @@
       0
       y))
 
-; Applicative:
+; Normal:
 ; (test 0 (p))
 ; (if (= 0 0) 0 (p))
 ; (if :t 0 (p))
 ; 0
 
-; Normal:
+; Aplicative:
 ; (test 0 (p))
 ; (test 0 (p))
 ; (test 0 (p))
