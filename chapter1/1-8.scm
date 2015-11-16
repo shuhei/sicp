6b6841248d2fa5373cf280bb88536dd5acfbacbf
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-11-17 00:25:43 +0900

    Add a solution to exercise 1-8, the last one of 1.1!

---
+++ chapter1/1-8.scm
@@ -0,0 +1,12 @@
+(define (cube-root x)
+        (define (improve-rate guess)
+                (/ (- (improve guess) guess) guess))
+        (define (good-enough? guess)
+                (< (abs (improve-rate guess)) 0.0000001))
+        (define (improve guess)
+                (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+        (define (cube-root-iter guess)
+                (if (good-enough? guess)
+                    guess
+                    (cube-root-iter (improve guess))))
+        (cube-root-iter 1.0))
