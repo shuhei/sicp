d1436669d5561d8d9c3c0175b667d91f52d174bf
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-11-27 20:49:43 +0900

    Add corrected answer of 1.14.

--- chapter1/1-14.scm
+++ chapter1/1-14.scm
@@ -15,5 +15,21 @@
               ((= kinds-of-coins 4) 25)
               ((= kinds-of-coins 5) 50)))
 
-; space == steps
-; Θ(n ^ 2) ?
+; space: O(n)
+; steps: Θ(n ^ 2) ?
+
+; -- Answer by Torii-san.
+;
+; steps, space:
+; (cc n 1) ∈ Θ(n)
+;
+; steps:
+; (cc n 2) = (cc n 1) + (cc (n - 5) 2) + 1
+;          = (cc n 1) + (cc (n - 5) 1) + (cc (n - 10) 2) + 2
+;          <= (cc n 1) + n/5 * (cc n 1) + n/5
+;          ∈ O(n^2)
+; (cc n 3) = (cc n 2) + ...
+;          ∈ O(n^3)
+;
+; space:
+; O(n)
