af516541b301db5657098a5290c738035c162de0
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-11-29 01:46:16 +0900

    Add analysis of 1.20, normal-order evaluation of GCD.

--- chapter1/1-20.scm
+++ chapter1/1-20.scm
@@ -4,6 +4,18 @@
       (gcd b (remainder a b))))
 
 ; Normal-order: 18 times
+;
+; Let's say A(n) and B(n) as numbers of remainder calls in `a`/`b` at nth call of `gcd`.
+; A(0) = 0
+; B(0) = 0
+; A(n + 1) = B(n)
+; B(n + 1) = 1 + A(n) + B(n) = 1 + B(n - 1) + B(n)
+;
+; T(n), the total number of remainder calls in n gcd calls, is:
+; T(n) = Σ(n)(B(k)) + A(n)
+;
+; B(n) = Fib(n) + 2(n - 2)
+; A(n) = Fib(n - 1)  + 2(n - 3)
 (gcd 206 40)
 (gcd 40 (remainder 206 40))
 (if (= (remainder 206 40) 0)
