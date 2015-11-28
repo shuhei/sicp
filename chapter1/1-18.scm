97ec515f59f9e7b1d2725ff8bcaa59b729829793
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-11-28 13:34:03 +0900

    Improve 1.18 using invariant quantity.

--- chapter1/1-18.scm
+++ chapter1/1-18.scm
@@ -4,11 +4,17 @@
 (define (double n)
         (+ n n))
 
+; TODO: Bit-shifting?
+(define (halve n)
+        (/ n 2))
+
 (define (iter-* a b)
-  (define (*-iter c m)
-          (cond ((= m b) c)
-                ((or (= m 0) (> (double m) b)) (*-iter (+ a c) (+ m 1)))
-                (else (*-iter (double c) (double m)))))
-  (if (< b 0)
-      (iter-* (- a) (- b))
-      (*-iter 0 0)))
+        ; `c + a * b` is the invariant quantity.
+        (define (*-iter c a b)
+                (cond ((= 0 b) c)
+                      ((even? b) (*-iter c (double a) (halve b)))
+                      (else (*-iter (+ c a) a (- b 1)))))
+        ; Handle negative b.
+        (if (< b 0)
+            (iter-* (- a) (- b))
+            (*-iter 0 a b)))
