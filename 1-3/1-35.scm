80190c793e8f367332836714fd0e52c387e95e55
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2016-01-20 08:58:21 +0900

    Exercise 1.35 and 1.36.

---
+++ 1-3/1-35.scm
@@ -0,0 +1,21 @@
+; x ^ 2 = x + 1
+; x = 1 + 1 / x
+
+(define (average x y)
+  (/ (+ x y) 2.0))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+; Golden ratio!
+(fixed-point (lambda (x) (average x (+ 1.0 (/ 1.0 x)))) 1.0)
